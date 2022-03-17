329a29016a1bbbaba2bf2003415c51e325735024
jeivison <jeivison1@hotmail.com>
2022-03-16 23:01:54 -0300

    Start project!

---
+++ app/src/main/java/com/example/jsnews/service/NewsServiceAPI.kt
@@ -0,0 +1,16 @@
+package com.example.jsnews.service
+
+import retrofit2.http.GET
+
+
+
+interface NewsServiceAPI {
+
+    /*
+    //https://newsapi.org/v2/top-headlines?country=us&apiKey=API_KEY
+    //KEY: 883503dde64a44e7a9f83ff2b1b4959e
+
+    @GET("")
+    fun getNewsAPI() :*/
+
+}
