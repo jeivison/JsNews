bdc24845e7a5a9e36917f5cb7a1d2718524d8479
jeivison <jeivison1@hotmail.com>
2022-03-17 15:42:17 -0300

    Start project2!

--- app/src/main/java/com/example/jsnews/NewsActivity.kt
+++ app/src/main/java/com/example/jsnews/NewsActivity.kt
@@ -2,21 +2,16 @@ package com.example.jsnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.main_news.*
 
 class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_news)
-        /*
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, EconomicFragment.newInstance())
-                .commitNow()
-        }*/
-        /*
-        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
-        viewPager.adapter = adapter*/
 
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
